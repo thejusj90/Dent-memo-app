62cf47df5079f14fee2d0ae19fe348f6374a30d7
thejusj90 <73927323+thejusj90@users.noreply.github.com>
2026-08-12 14:35:43 +0530

    Fix login flow for Google accounts

--- lib/supabase/auth.ts
+++ lib/supabase/auth.ts
@@ -17,7 +17,19 @@ export async function signIn(email: string, password: string) {
   return getSupabaseBrowserClient().auth.signInWithPassword({ email, password });
 }
 
+export async function signInWithGoogle() {
+  return getSupabaseBrowserClient().auth.signInWithOAuth({
+    provider: "google",
+    options: { redirectTo: window.location.href.split("#")[0] },
+  });
+}
+
 export async function signOut() {
   return getSupabaseBrowserClient().auth.signOut();
 }
 
+export async function getSignedInUser() {
+  const { data, error } = await getSupabaseBrowserClient().auth.getUser();
+  if (error) throw error;
+  return data.user;
+}
