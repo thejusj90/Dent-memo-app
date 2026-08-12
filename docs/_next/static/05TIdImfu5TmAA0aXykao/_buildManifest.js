cf213ef335f8fda78d7f09b51f2b8d92a228e4ad
thejusj90 <73927323+thejusj90@users.noreply.github.com>
2026-08-12 14:52:47 +0530

    Publish built DentMemo app in docs

---
+++ docs/_next/static/05TIdImfu5TmAA0aXykao/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Dent-memo-app//_next/:path+",
+        "destination": "/Dent-memo-app/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
